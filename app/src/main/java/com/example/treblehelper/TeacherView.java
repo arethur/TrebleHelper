a393992f72a06323e262f1e985086b2a7cda0d32
arethur <tro17001@byui.edu>
2019-04-03 09:54:59 -0600

    worked on it.

--- app/src/main/java/com/example/treblehelper/TeacherView.java
+++ app/src/main/java/com/example/treblehelper/TeacherView.java
@@ -11,14 +11,25 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TeacherView extends AppCompatActivity {
 
-    private Teacher teacher;
+    private Teacher User;
     private ArrayList<Student> studentList;
     private ListView SL;
+    private Map<String, Users> userMap;
+    private static final String PREFS = "MAP_LOCATION";
+    private static final String MAP_TEACHER = "MAP_TEACHER";
+    private Gson gson = new Gson();
+
 
 //    public TeacherView(Teacher teachers1){
 //        this.teacher = teachers1;
@@ -31,52 +42,49 @@ public class TeacherView extends AppCompatActivity {
         setContentView(R.layout.activity_teacher_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        String stringTeach;
+        String stringTeach = null;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras == null) {
                 stringTeach = null;
             } else {
                 stringTeach = extras.getString("teacher");
+                User= gson.fromJson(stringTeach, Teacher.class);
             }
-        }
-
-        if (teacher != null) {
-            studentList = (ArrayList<Student>) teacher.students;
         }
 
         TextView name = findViewById(R.id.textView2);
-        name.setText(teacher.getFirstName() + " " + teacher.getLastName());
+        name.setText(User.getFirstName() + " " + User.getLastName());
 
         Log.i("Listviews", "Starting on list views.");
-
-        Announcements announcements = new Announcements(this);
-        announcements.viewAnnouncements();
-
-        Log.d("AnoucementsList", "Announcement listView was made.");
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        SL = SL.findViewById(R.id.studentslist);
-        SL.setAdapter(arrayAdapter);
-
-        for(Users students : studentList) {
-            String outlook = " ";
-            if (studentList.size() > 0) {
-                outlook = String.valueOf(studentList.get(0));
-            }
-            arrayAdapter.add(outlook);
-        }
-
-        Log.d("StudentsList","Student ListView was made");
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//
+//        Announcements announcements = new Announcements(this);
+//        announcements.viewAnnouncements();
+//
+//        Log.d("AnoucementsList", "Announcement listView was made.");
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+//        SL = SL.findViewById(R.id.studentslist);
+//        SL.setAdapter(arrayAdapter);
+//
+//        for(Users students : studentList) {
+//            String outlook = " ";
+//            if (studentList.size() > 0) {
+//                outlook = String.valueOf(studentList.get(0));
+//            }
+//            arrayAdapter.add(outlook);
+//        }
+//
+//        Log.d("StudentsList","Student ListView was made");
+//
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
     }
 
